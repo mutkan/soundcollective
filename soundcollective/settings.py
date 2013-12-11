9b9d2e4e24a46881178d8acbfce62a3040763251
bryant <sirwiggle@gmail.com>
2013-12-10 22:07:15 -0800

    s3 setup

--- soundcollective/settings.py
+++ soundcollective/settings.py
@@ -107,7 +107,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_SOUND']
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Heroku
