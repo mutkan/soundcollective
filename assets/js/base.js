a75d0a998a1d1ac7b0c324e4ed6c895a6f264d8b
bryant <sirwiggle@gmail.com>
2014-01-19 20:04:28 -0800

    saving

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,2 +1,1 @@
-$("#profile-column-2").height($("#profile-column-1").height());
-$("#testing-image").height($("#profile-column-1").height() - 42);
+$("#profile-column-1").height($("#profile-column-2").height());
